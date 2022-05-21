f048b6bb17b206b69579b9120a6d0e3575170ecd
roberto di lillo <dilillo.roberto@gmail.com>
2022-05-21 23:19:32 +0100

    adds logic

--- src/components/DrumMachine/DrumMachine.js
+++ src/components/DrumMachine/DrumMachine.js
@@ -1,94 +1,115 @@
 import './DrumMachine.css';
-// import { useState, useRef } from 'react';
-// const alarm = require('./../../resources/alarm.wav');
+import { useState, useEffect } from 'react';
+
+const sound1 = require('./../../resources/Cev_H2.mp3');
+const sound2 = require('./../../resources/Dsc_Oh.mp3');
+const sound3 = require('../../resources/Heater1.mp3');
+const sound4 = require('./../../resources/Heater-2.mp3');
+const sound5 = require('./../../resources/Heater-3.mp3');
+const sound6 = require('./../../resources/Heater-4_1.mp3');
+const sound7 = require('./../../resources/Heater-6.mp3');
+const sound8 = require('./../../resources/Kick_n_Hat.mp3');
+const sound9 = require('./../../resources/RP4_KICK_1.mp3');
 
 function DrumMachine() {
-	// const audRef = useRef(null);
 
-	// const [settings, setSettings] = useState({session: 25, break: 5});
-	// const [timer, setTimer] = useState({
-	// 	minutes: '25', 
-	// 	seconds: '00', 
-	// 	active: false, 
-	// 	isBreak: false
-	// });
-	
-	// const playAudio = () => {
-	// 	console.log('here', audRef)
-	// 	audRef.current.play();
-	// }
+	const [display, setDisplay] = useState('');
+	const sounds = [
+		{
+            keyCode: 67,
+            keyTrigger: "C",
+            id: "Closed-HH",
+			sound: sound1
+        },
+		{
+            keyCode: 68,
+            keyTrigger: "D",
+            id: "Open-HH",
+			sound: sound2
+        },
+        {
+            keyCode: 81,
+            keyTrigger: "Q",
+            id: "Heater-1",
+			sound: sound3
+        },
+        {
+            keyCode: 87,
+            keyTrigger: "W",
+            id: "Heater-2",
+			sound: sound4
+        },
+        {
+            keyCode: 69,
+            keyTrigger: "E",
+            id: "Heater-3",
+			sound: sound5
+        },
+        {
+            keyCode: 65,
+            keyTrigger: "A",
+            id: "Heater-4",
+			sound: sound6
+        },
+        {
+            keyCode: 83,
+            keyTrigger: "S",
+            id: "Clap",
+			sound: sound7
+        },
+        {
+            keyCode: 90,
+            keyTrigger: "Z",
+            id: "Kick-n-Hat",
+			sound: sound8
+        },
+        {
+            keyCode: 88,
+            keyTrigger: "X",
+            id: "Kick",
+			sound: sound9
+        }
+    ]  
 
-	  
-	// const toggleTimer = (value) => {
-	// 	setTimer({ ...timer, active: value})
-	// }
+	const onKeyDownhandler = ({keyCode}) => {
+		const item = sounds.find( item => item.keyCode === keyCode);
+		if(item) {
+			playSound(item)
+		}
+	};
 
-	// const refreshTimer = () => {
-	// 	audRef.current.pause();
-	// 	setTimer({ 
-	// 		minutes: '25',
-	// 		seconds: '00', 
-	// 		active: false,
-	// 		isBreak: false
-	// 	})
-	// 	setSettings({session: 25, break: 5});
-	// }
+	useEffect(() => {
+		document.addEventListener('keydown', onKeyDownhandler);
+		return () => {
+		  document.removeEventListener('keydown', onKeyDownhandler);
+		};
+	}, []);
 
-	// const editSettings =  (type, val) => {
-	// 	if(timer.active) return
-	// 	const newSettings = { ...settings }
-	// 	newSettings[type] +=val;
-	// 	if(newSettings[type] > 60) {
-	// 		newSettings[type] = 60
-	// 	}
-	// 	if(newSettings[type] < 1) {
-	// 		newSettings[type] = 1
-	// 	}
-	// 	setSettings(newSettings)
-	// 	if(type === 'session' && !timer.isBreak) {
-	// 		setTimer({
-	// 			...timer,
-	// 			minutes: newSettings[type],
-	// 			seconds: '00'
-	// 		})
-	// 	}
-		
-	// }
+	const playSound = (item) => {
+		const sound = document.getElementById(item.id);
+		sound.currentTime = 0;
+		sound.play();
+		setDisplay(item.id)
+	}
+
+	const pads = sounds.map( (item, idx) => {
+		return <div 
+			id={item.keyTrigger} 
+			className="drum-pad" 
+			key={item.keyTrigger} 
+			onClick={()=>playSound(item)}
+			tabIndex={idx}
+		>
+			<span>{item.id}</span>
+			
+			<audio className="clip" id={item.id} src={item.sound} hidden />
+		</div>
+	})
 
 	return (
-		<div className="DrumMachine">
-
-			{/* <h2 className="title" id="timer-label">{settings.isBreak ? 'Break' : 'Session'}</h2>
-			<div className="session-container">
-				<div className='session-value' id="time-left">{timer.minutes}:{timer.seconds}</div>
-				<div className='controls-container'>
-					<span className='icon-container'>
-					</span>
-					<span className='icon-container'>
-					</span>
-					<span className='icon-container'>
-					</span>
-				</div>
-			</div>
-
-			<h2 className="title" id="session-label">Session Length</h2>
-			<div className="setting-container">
-				<span className='icon-container' id="session-decrement" onClick={()=> editSettings('session', -1)}>
-				</span>
-				<div className="setting-value" id="session-length">{settings.session}</div>
-				<span className='icon-container' id="session-decrement" onClick={()=> editSettings('session', 1)}>
-				</span>
-			</div>
-			
-			<h2 className="title" id="break-label">Break Length</h2>
-			<div className="setting-container">
-				<span className='icon-container' id="break-decrement" onClick={()=> editSettings('break', -1)}>
-				</span>
-				<div className="setting-value">{settings.break}</div>
-				<span className='icon-container' id="break-increment" onClick={()=> editSettings('break', +1)}>
-				</span>
-			</div>
-			<audio id="beep" src={alarm} ref={audRef} hidden /> */}
+		<div className="DrumMachine" id="drum-machine">
+			<div id="display"></div>
+			{pads}
 		</div>
 		);
 	}
